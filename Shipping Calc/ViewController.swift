141dfa7b499b7383db69a8fe2502cb55a0841426
Ryan Besemer <rbesemer@gmail.com>
2018-05-06 23:55:54 -0700

    Initial Commit

---
+++ Shipping Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shipping Calc
+//
+//  Created by Ryan Besemer on 5/6/18.
+//  Copyright © 2018 Ryan Besemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
